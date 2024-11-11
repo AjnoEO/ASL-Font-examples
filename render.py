e0b0785e91398583a32dc7572413ef01e473c358
Ajno <boyko.artiom@gmail.com>
2024-11-12 01:28:03 +0300

    Added links and better categorization

--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup, Tag
 import json
 from enum import Enum, auto
+import re
 
 class HandshapeBalance(Enum):
     NO_HANDSHAPES = auto()
@@ -16,6 +17,9 @@ def is_handshape(char: str):
 def handshapes_balance(string: str) -> HandshapeBalance:
     hs = False
     non_hs = False
+    if re.match(r"^\{(\.\.?|``?)?[A-IK-Ya-z0-9](\.\.?|``?|[<>_^/Z])?\}[A-IK-Ya-z0-9]?$", string):
+        # Spelling of location relative to the non-dom hand
+        return HandshapeBalance.NO_HANDSHAPES
     for char in string:
         if char.isspace(): continue
         if is_handshape(char): hs = True
@@ -26,35 +30,86 @@ def handshapes_balance(string: str) -> HandshapeBalance:
         return HandshapeBalance.HANDSHAPES_ONLY
     return HandshapeBalance.BOTH_PRESENT
 
-def create_tag(soup: BeautifulSoup, name: str, attrs: dict = {}, string: str | list[str] | None = None):
+def create_tag(soup: BeautifulSoup, name: str, attrs: dict = {}, string: str | None = None):
     tag = soup.new_tag(name, attrs=attrs)
-    if string:
-        for s in string.split("<br/>"):
-            tag.append(s)
-            tag.append(soup.new_tag("br"))
+    if string: tag.append(BeautifulSoup(string, "html.parser"))
     return tag
 
-def main():
+def asl_dict_link(word: str):
+    ASL_DICT_URL = "https://www.signasl.org/sign/"
+    SUBS = {
+        "'": "",
+        " ": "-",
+        "5": "five"
+    }
+    url = word
+    for old, new in SUBS.items():
+        url = url.replace(old, new)
+    url = ASL_DICT_URL + url
+    return f'<a href="{url}">{word}</a>'
+
+def deconfixicate(string: str) -> tuple[str, str, str]:
+    if string[-1] == ".": return "", string, ""
+    suffix_prefixes = [", ", " on ", " ("]
+    for s in suffix_prefixes:
+        pos = string.find(s)
+        if pos > -1:
+            prefix, root, suffix = deconfixicate(string[:pos])
+            return prefix, root, suffix + string[pos:]
+    prefix_suffixes = [") "]
+    for p in prefix_suffixes:
+        pos = string.find(p)
+        if pos > -1:
+            pos = pos + len(p)
+            prefix, root, suffix = deconfixicate(string[pos:])
+            return string[:pos] + prefix, root, suffix
+    return "", string, ""
+
+def create_links(string: str):
+    prefix, root, suffix = deconfixicate(string)
+    if (pos := root.find("/")) > -1:
+        root = asl_dict_link(root[:pos]) + "/" + asl_dict_link(root[pos+1:])
+    else:
+        root = asl_dict_link(root)
+    return prefix + root + suffix
+
+def fetch_tranlations() -> dict[str, list[tuple[str, str]]]:
     with open("translations.json", encoding="utf8") as f:
         LOADED_TRANSLATIONS: dict[str, list[str]] = json.load(f)
     
-    translations: dict[str, list[tuple[str, str]]] = {"Lexicon": [], "Suspiciously simplified": [], "Symbols": []}
-
+    TITLES_LINKS = ["Lexicon", "Suspiciously simplified"]
+    TITLES_NO_LINKS = ["Phrases", "Symbols"]
+    translations: dict[str, dict[str, set[str]]] = {title: {} for title in TITLES_LINKS + TITLES_NO_LINKS}
+    
     for meaning, transcriptions in LOADED_TRANSLATIONS.items():
-        if not meaning or meaning.isspace():
-            for t in transcriptions:
-                translations["Symbols"].append((t, meaning))
-            continue
-        transcription = "<br/>".join(transcriptions)
-        hs_b = handshapes_balance(transcription)
-        entry = (transcription, meaning)
-        if len(transcription) == 1: translations["Symbols"].append(entry)
-        elif hs_b == HandshapeBalance.NO_HANDSHAPES: translations["Symbols"].append(entry)
-        elif hs_b == HandshapeBalance.HANDSHAPES_ONLY: translations["Suspiciously simplified"].append(entry)
-        else: translations["Lexicon"].append(entry)
+        m = deconfixicate(meaning)[1] # Extract root
+        for t in transcriptions:
+            hs_b = handshapes_balance(t)
+            if len(t) > 1 and " " not in t and hs_b == HandshapeBalance.HANDSHAPES_ONLY:
+                key = "Suspiciously simplified"
+                m = m.lower()
+            elif not m or m.isspace() or handshapes_balance(t) != HandshapeBalance.BOTH_PRESENT:
+                key = "Symbols"
+            elif (" " in t and " " in m):
+                key = "Phrases"
+            else:
+                key = "Lexicon"
+            if key in TITLES_LINKS:
+                meaning = meaning.lower()
+                if meaning[-1] == "?": meaning
+            translations[key].setdefault(meaning, set())
+            translations[key][meaning].add(t)
+
+    return {
+        title: [("<br/>".join(t), create_links(m) if title in TITLES_LINKS else m) for m, t in translations[title].items()]
+        for title in translations
+    }
+
+def main():
+    translations = fetch_tranlations()
     
     for l in translations.values():
-        l.sort(key=lambda e: e[1].lower())
+        l.sort(key=lambda e: deconfixicate(e[1])[1].lower())
 
     with open("base.html", encoding="utf8") as f:
         BASE_HTML = f.read()
@@ -63,7 +118,7 @@ def main():
     insert: Tag = soup.find(attrs={"id": "sections"})
     for title, contents in translations.items():
         insert.append(create_tag(soup, "h4", {}, title))
-        s_contents = create_tag(soup, "dl", {"class": "inline inline2b"})
+        s_contents = create_tag(soup, "dl", {"class": "inline inline2d" if title != "Phrases" else "inline inline1c"})
         for asl, en in contents:
             s_contents.append(create_tag(soup, "dt", {"class": "asl"}, asl))
             s_contents.append(create_tag(soup, "dd", {}, en))
@@ -72,6 +127,6 @@ def main():
         insert.append(s_content_box)
     
     with open("index.html", mode="w", encoding="utf8") as f:
-        f.write(soup.prettify())
+        f.write(str(soup))
 
 if __name__ == '__main__': main()
