90036fd4ed581a672861c371455acf82edf833d6
Ajno <boyko.artiom@gmail.com>
2024-11-12 01:27:25 +0300

    Fixed some examples being missed. Better unification

--- fetch.py
+++ fetch.py
@@ -9,10 +9,14 @@ from random import random
 BASE_URL = "https://aslfont.github.io/Symbol-Font-For-ASL/"
 
 def unify(text: str):
-    suffixes_to_remove = [" (with orientation)", " (without)"]
+    text = text.strip()
+    suffixes_to_remove = ["(with orientation)", "(without)", "(abbreviation)"]
     for s in suffixes_to_remove:
         if text.endswith(s):
             text = text[:-len(s)]
+            text.strip()
+    if text and text[0] == '"' and (second_quote := text.find('"', 1)) > -1 and text[1].isalnum():
+        text = text[1:second_quote]
     return text
 
 
@@ -24,43 +28,49 @@ def main(test: bool = False):
 
     urls_to_check = SimpleQueue()
     urls_to_check.put(BASE_URL)
-    checked_urls = {BASE_URL}
+    checked_urls = set()
     translations: dict[str, set[str]] = {}
 
     print("> START")
 
     while not urls_to_check.empty() and counter != 0:
+        current_url = urls_to_check.get()
+        if current_url in checked_urls:
+            continue
         counter -= 1
         user_agent = UserAgent().random
-        current_url = urls_to_check.get()
         response = requests.get(current_url, headers={'User-Agent': user_agent})
         if current_url == response.url:
             print(response.url, response.status_code)
         else:
             print(current_url, ">", response.url, response.status_code)
             current_url = response.url
+            if current_url in checked_urls:
+                print("(skipping)")
+                continue
+        checked_urls.add(current_url)
         current_url = current_url[:current_url.rfind('/')+1]
         if response.status_code != 200:
             continue
         soup = BeautifulSoup(response.text, 'html.parser')
         for example_block in soup.find_all('dl', {'class': 'inline'}):
             example_block: Tag
-            last_asl = last_en = None
-            for tag in example_block.children:
-                tag: Tag
-                if tag.name == 'dt' and 'class' in tag.attrs and 'asl' in tag.attrs['class']:
-                    last_asl = tag.get_text()
-                    continue
-                elif tag.name == 'dd' and last_asl:
-                    last_en = unify(tag.get_text())
-                    translations.setdefault(last_en, set())
-                    translations[last_en].add(last_asl)
-                last_asl = last_en = None
+            for this_tag in example_block.find_all('dt', {'class': 'asl'}):
+                this_tag: Tag
+                next_tag: Tag | None = this_tag.next_sibling
+                if next_tag and not next_tag.name:
+                    next_tag = next_tag.next_sibling
+                if next_tag and next_tag.name == 'dd':
+                    asl = this_tag.get_text()
+                    en = unify(next_tag.get_text())
+                    if not en:
+                        continue
+                    translations.setdefault(en, set())
+                    translations[en].add(asl)
         print(f"Translated terms: {len(translations.keys())}")
         for link in soup.find_all('a'):
             link: Tag
             url: str = link["href"]
-            # print(">", link.get_text(), url)
             if '#' in url:
                 continue
             if not url.startswith(('https://', 'http://')):
@@ -70,8 +80,7 @@ def main(test: bool = False):
             if url in checked_urls:
                 continue
             urls_to_check.put(url)
-            checked_urls.add(url)
-        sleep(1+2*random())
+        sleep(2*random())
 
     with open("translations.json", mode="w", encoding="utf8") as f:
         translations = {k: list(v) for k, v in translations.items()}
